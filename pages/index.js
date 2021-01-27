eac076868ea17362c9f7c8f72ddcb982e1ed1d0d
Raphael-GC <raphaelgc.i9@gmail.com>
2021-01-26 23:13:38 -0300

    Day 01 + Div alignment

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,10 @@ export const QuizContainer = styled.div`
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
-  margin-left: 570px; // Para centralizar a Div na tela.
+  margin-left: 570px; // Para centralizar horizontalmente a Div na tela.
+  margin-top: 30px;  // Para centralizar horizontalmente a Div na tela.
   @media screen and (max-width: 500px) {
-    margin: auto;
+    margin: 225px auto 5px auto; // Para tirar a Div de cima do Background no mobile.
     padding: 15px;
   }
 `;
