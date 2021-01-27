536b3d7a4711f616bb0d24000b8c7fd1ea868b64
Raphael-GC <raphaelgc.i9@gmail.com>
2021-01-27 11:31:54 -0300

    Head adjustments

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Head from 'next/head'
-//import Link from 'next/link'     (Para fazer apontamento entre as paginas)
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+//import Link from 'next/link'     (Para fazer apontamento entre as paginas);
 
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 
 //---------Background anterior nao responsivo-------------//
@@ -51,13 +51,14 @@ export default function Home() {
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={db.bg} />
         <meta property="twitter:title" content={db.title} />
-        <meta property="twitter:description" content={db.description} >
+        <meta property="twitter:description" content={db.description} />
         <meta property="twitter:image" content={db.bg} />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
+
       <QuizContainer>
         <QuizLogo/>
         <Widget>
@@ -79,7 +80,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl={db.gitUrl} />
+      <GitHubCorner projectUrl={db.gitUrl} />  
     </QuizBackground>
   )
 }
